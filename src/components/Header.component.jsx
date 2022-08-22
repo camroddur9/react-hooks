667facf50b9dd047c3c19de9cb09637ea0b220c3
camroddur9 <ui.crodriguezd@gmail.com>
2022-08-21 21:12:18 -0500

    UseContext Class

--- src/components/Header.component.jsx
+++ src/components/Header.component.jsx
@@ -1,8 +1,11 @@
-import React, { useState } from 'react'
+import React, { useState, useContext } from 'react'
+import ThemeContext from '../context/ThemeContext';
 
 const Header = () => {
 
     const [darkMode, setDarkmode] = useState(false);
+
+    const color = useContext(ThemeContext)
 
     const handleClick = () => {
         setDarkmode(!darkMode) 
@@ -10,7 +13,7 @@ const Header = () => {
 
     return(
         <div className="Header">
-            <h1>ReactHooks</h1>
+            <h1 style={{color: color}}>ReactHooks</h1>
             <button type='button' onClick={() => {handleClick()}}>{darkMode ? "Dark Mode":"Light Mode"}</button>
         </div>
     )
